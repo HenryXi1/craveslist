294cc9bc07c913fb60057499a12ca13bd0b16e83
GcWan <GcWan@users.noreply.github.com>
2021-10-15 23:06:40 -0400

    Change ingredients.py to a function

--- ingredients.py
+++ ingredients.py
@@ -7,28 +7,30 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-searchTerms = input("What food do you want to make?\n")
-service = Service(ChromeDriverManager().install())
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-browser = webdriver.Chrome(service=service, options=chrome_options)
-browser.maximize_window()
-browser.get("https://www.yummly.com/recipes")
+def getIngredients(search_Terms):
+    service = Service(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    browser = webdriver.Chrome(service=service, options=chrome_options)
+    browser.maximize_window()
+    browser.get("https://www.yummly.com/recipes")
 
-WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.ID, "searchbox-input")))
-searchBar = browser.find_element(By.ID, "searchbox-input")
-searchBar.send_keys(searchTerms)
-searchBar.send_keys(Keys.ENTER)
+    WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.ID, "searchbox-input")))
+    searchBar = browser.find_element(By.ID, "searchbox-input")
+    searchBar.send_keys(search_Terms)
+    searchBar.send_keys(Keys.ENTER)
 
-WebDriverWait(browser, 1).until(EC.presence_of_element_located((By.XPATH, "//div[@class='recipe-card-img-wrapper']")))
-firstItem = browser.find_element(By.XPATH, "//div[@class='recipe-card-img-wrapper']").find_element(By.TAG_NAME, "a")
-browser.execute_script("arguments[0].click();", firstItem)
+    WebDriverWait(browser, 1).until(EC.presence_of_element_located((By.XPATH, "//div[@class='recipe-card-img-wrapper']")))
+    firstItem = browser.find_element(By.XPATH, "//div[@class='recipe-card-img-wrapper']").find_element(By.TAG_NAME, "a")
+    browser.execute_script("arguments[0].click();", firstItem)
 
-ingredients = browser.find_elements(By.XPATH, "//span[@class='ingredient']")
-ingredients_list = []
-for k in range(len(ingredients)):
-    ingredients_list.append(ingredients[k].text)
+    WebDriverWait(browser, 1).until(EC.presence_of_element_located((By.XPATH, "//span[@class='ingredient']")))
+    ingredients = browser.find_elements(By.XPATH, "//span[@class='ingredient']")
+    ingredients_list = []
+    for k in range(len(ingredients)):
+        ingredients_list.append(ingredients[k].text)
 
-for k in ingredients_list:
-    print(k)
+    for k in ingredients_list:
+        print(k)
 
+    return ingredients_list
