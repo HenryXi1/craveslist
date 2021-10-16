a1700f895e87626b9f0d4ade2c883837ca067c39
GcWan <GcWan@users.noreply.github.com>
2021-10-15 21:23:40 -0400

    Add testing commands to ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
-searchTerms = "Lemon Shrimp Pasta"
-browser = webdriver.Chrome
-browser.get("https://www.allrecipes.com/")
-search = browser.find_element_by_id("primary-search")
-search.send_keys(searchTerms)
-searchButton = browser.find_element_by_title("Search")
-searchButton.click()
+searchTerms = "Tuscan Chicken Pasta"
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.maximize_window()
+browser.get("https://www.yummly.com/")
+# searchToggle = browser.find_element_by_id("search-modal-searchbox-input")
+'''
+searchBar = browser.find_element_by_id("search-modal-searchbox-input")
+searchBar.send_keys(searchTerms)
+searchBar.send_keys(Keys.ENTER)
+'''
 wait = WebDriverWait(browser, 5)
 print(browser.title)
