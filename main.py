3851f71dacf2eb9886ef0f970adb7c7d668c277d
GcWan <GcWan@users.noreply.github.com>
2021-10-16 23:44:20 -0400

    Added loading gif to flask app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ingredients import getIngredients, getPrice
+import aisleRead
 from send import send_receive
 import asyncio
 from flask import Flask, render_template, request, escape, Response
@@ -8,34 +9,29 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        item = str(escape(request.args.get("item", "")))
-        if item:
-            food_info = getIngredients(item)
-            price_info = getPrice(food_info['ingredients'])
-            return (
-                render_template(
-                    "index.html",
-                    result=[[food_info['ingredients'][i], price_info[i]] for i in range(len(price_info))],
-                    name="Ingredients for: " + food_info['name'],
-                    time="Approximate preparation time: " + food_info['time'],
-                    nutrition="Estimated Calories: " + food_info['nutrition']
-                )
-            )
-    elif request.method == 'POST':
+        if 'submit_button' in request.args:
+            item = str(escape(request.args.get("item", "")))
+            if not item:
+                return render_template("index.html", time="Please enter a search term")
+        else:
+            return render_template("index.html")
+    else:
         print("Listening...")
         item = asyncio.run(send_receive("ingredients"))
-        food_info = getIngredients(item)
-        price_info = getPrice(food_info['ingredients'])
-        return (
-            render_template(
-                "index.html",
-                result=[[food_info['ingredients'][i], price_info[i]] for i in range(len(price_info))],
-                name="Ingredients for: " + food_info['name'],
-                time="Approximate preparation time: " + food_info['time'],
-                nutrition="Estimated Calories: " + food_info['nutrition']
-            )
+    food_info = getIngredients(item)
+    ingredients = food_info['ingredients']
+    price_info = getPrice(ingredients)
+    aisle_info = [aisleRead.find(k) for k in ingredients]
+
+    return (
+        render_template(
+            "index.html",
+            result=[[ingredients[i], price_info[i], aisle_info[i]] for i in range(len(price_info))],
+            name="Ingredients for: " + food_info['name'],
+            time="Approximate preparation time: " + food_info['time'],
+            nutrition="Estimated Calories: " + food_info['nutrition'],
         )
-    return render_template("index.html")
+    )
 
 # @app.route('/audio')
 # def audio():
